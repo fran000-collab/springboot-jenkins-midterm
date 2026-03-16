ec5dad801d7b677b5409ccccae0f116c9574e423
Rene <renefrivas000@gmail.com>
2026-03-16 15:04:31 -0400

    Initial Spring Boot project

---
+++ src/main/java/montclair/edu/midterm/Application.java
@@ -0,0 +1,13 @@
+package montclair.edu.midterm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/montclair/edu/midterm/ApplicationTests.java
@@ -0,0 +1,13 @@
+package montclair.edu.midterm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
